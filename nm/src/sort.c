3e9e323735adf90d310e9ecf3dc311becc908e83
Adubedat <arthur.dubedat@gmail.com>
2018-03-01 16:38:06 +0100

    change sort first elem

--- nm/src/sort.c
+++ nm/src/sort.c
@@ -6,7 +6,7 @@
 /*   By: adubedat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 13:14:53 by adubedat          #+#    #+#             */
-/*   Updated: 2018/02/26 14:16:07 by adubedat         ###   ########.fr       */
+/*   Updated: 2018/03/01 16:36:46 by adubedat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	lstadd_ascii_sorted(t_sym_list **list, t_sym_list *new)
 	t_sym_list	*temp;
 
 	temp = *list;
-	if (temp == NULL || ft_strcmp(temp->name, new->name) > 0)
+	if (temp == NULL || ft_strcmp(temp->name, new->name) > 0
+		|| (ft_strcmp(temp->name, new->name) == 0 && temp->value > new->value))
 	{
 		new->next = *list;
 		*list = new;
